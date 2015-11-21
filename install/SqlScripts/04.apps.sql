496457823d3bb9b84c538741d8b8c73f0f07712e
yuki <yuki@gakkougurashi.net>
2015-11-21 11:42:02 +0900

    Add Tables Version 2 (Add Token Tables)

---
+++ install/SqlScripts/04.apps.sql
@@ -0,0 +1,21 @@
+-- init apps table
+
+CREATE TABLE `Apps`(
+  `appid` INT AUTO_INCREMENT PRIMARY KEY COMMENT 'App ID',
+  `appname` VARCHAR(32) NOT NULL UNIQUE COMMENT 'App Name',
+  `secret` VARCHAR(32) BINARY COLLATE 'utf8_bin' NOT NULL COMMENT 'App Secret',
+  `connecttime` TIMESTAMP NOT NULL COMMENT 'Connect Time',
+  `status` INT DEFAULT 0 COMMENT 'Connect Status'
+) COMMENT 'Apps'
+
+-- init apps end
+/*
+
+ Connect Status (来源显示只适合于主认证系统是社交平台的情况):
+  0 - Developing (显示: 来自未经审核的应用)
+  1 - Waiting for Review (显示: 来自未经审核的应用)
+  2 - Review Rejected (显示: 来自未经审核的应用)
+  3 - Review Accepted (显示: 来自应用)
+  4 - Online (显示: 来自应用)
+
+ */
