6ad123d11fef02e9111a94237893b5568f99a2d8
yuki <yuki@gakkougurashi.net>
2015-11-21 18:08:26 +0900

    Init An Empty JS API

---
+++ scripts/webapi.js
@@ -0,0 +1,20 @@
+(function(window,undefined){
+    var yukiauth={
+        check:function(key){
+
+        },
+        auth:function(key){
+
+        },
+        logout:function(key){
+
+        },
+        showinfo:function(key){
+
+        },
+        showlogin:function(key){
+
+        }
+    };
+    window.yukiauth=yukiauth;
+})(window);
