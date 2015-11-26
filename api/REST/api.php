841eeb85824a330484ec0ab439e8afccaadb0dda
yuki <yuki@gakkougurashi.net>
2015-11-26 11:50:31 +0900

    Add CSAPI

--- api/REST/api.php
+++ api/REST/api.php
@@ -1,4 +1,5 @@
 <?php
+include_once $_SERVER["DOCUMENT_ROOT"] . "/api/common/apicommon.php";
 include_once $_SERVER["DOCUMENT_ROOT"] . "/api/common/errcode.php";
 
 $path=$_SERVER["PATH_INFO"];
--- api/cURL/api.php
+++ api/cURL/api.php
@@ -1,4 +1,5 @@
 <?php
+include_once $_SERVER["DOCUMENT_ROOT"] . "/api/common/apicommon.php";
 include_once $_SERVER["DOCUMENT_ROOT"] . "/api/common/errcode.php";
 
 $api=$_POST["method"];
